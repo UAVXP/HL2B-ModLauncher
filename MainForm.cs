a07af782d9d3da8f7282ded9df2b0ca565446bfb
VXP <uavxp29@gmail.com>
2016-03-28 12:38:45 +0300

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+using System.IO;
+using Microsoft.Win32;
+
+namespace ModLauncher
+{
+	public partial class MainForm : Form
+	{
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HTCAPTION = 0x2;
+		[DllImport("User32.dll")]
+		public static extern bool ReleaseCapture();
+		[DllImport("User32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+		// Define the CS_DROPSHADOW constant
+		private const int CS_DROPSHADOW = 0x00020000;
+		// Override the CreateParams property
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ClassStyle |= CS_DROPSHADOW;
+				return cp;
+			}
+		}
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void close_Click(object sender, EventArgs e)
+		{
+		//	logout();
+			this.Close();
+		}
+
+		private void minimize_Click(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Minimized;
+		}
+
+		private void move_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+				ReleaseCapture();
+				SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+			}
+        }
+
+		public static bool isModDirectory(string path)
+		{
+			string[] ignoredirs = {
+								 "3dsmax2.5",
+								 "3dsmax3",
+								 "bin",
+								 "logos",
+								 "media",
+								 "pak",
+								 "Photoshop 5.5",
+								 "platform",
+								 "src_main",
+								 "XSI",
+							 };
+			string shortDir = path.Substring(path.LastIndexOf('\\') + 1);
+			foreach (string dir in ignoredirs)
+			{
+				if (dir == shortDir)
+					return false;
+			}
+
+			if (!File.Exists(path + "\\scripts\\liblist.gam"))
+			{
+				return false;
+			}
+
+			return true;
+		}
+		public static string getRegistryMainPath()
+		{
+			return "Software\\LeakNet";
+		}
+		public static bool isRegistryPathValid(string str)
+		{
+			RegistryKey key = Registry.CurrentUser.OpenSubKey(str);
+		//	Console.WriteLine("Checking registry path " + str);
+		//	if (key == null) return false;
+			if (key == null)
+			{
+			//	Console.WriteLine("Creating a new registry path "+str);
+				RegistryKey key2 = Registry.CurrentUser.CreateSubKey(str);
+				key2.Close();
+			}
+			key = Registry.CurrentUser.OpenSubKey(str);
+		//	Console.WriteLine("Key Count is " + key.ValueCount);
+			if (key.ValueCount > 0)
+			{
+				key.Close();
+				return true;
+			}
+			return false;
+		}
+		public static string getRegistryValue(string main, string subkey)
+		{
+			try
+			{
+				if (!isRegistryPathValid(getRegistryMainPath()))
+					return "";
+
+				RegistryKey key = Registry.CurrentUser.OpenSubKey(main);
+				string result = (string)key.GetValue(subkey);
+			//	if (String.Compare(result, "") == 0)
+			//		key.SetValue(subkey, "");
+				key.Close();
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Can't get registry value!");
+				return "";
+			}
+		}
+		public static bool setRegistryValue(string subkey, string value)
+		{
+		//	Console.WriteLine("Registry path/value is: " +
+		//						getRegistryMainPath() + "\\" +
+		//						subkey + "\\" +
+		//						value);
+			try
+			{
+
+				RegistryKey key = Registry.CurrentUser.CreateSubKey(getRegistryMainPath());
+				key.SetValue(subkey, value);
+				key.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return false;
+			}
+			return true;
+		}
+
+	//	public static string gamePath = Directory.GetCurrentDirectory();
+		
+#if DEBUG
+		public static string gamePath = @"D:\AHL2_R";
+#else
+		public static string gamePath = Directory.GetCurrentDirectory();
+#endif
+
+		public static string getModDirectory()
+		{
+		//	return getRegistryValue(FileSystem.Main.getRegistryMainPath(), "LNGameMod");
+			string regValue = getRegistryValue(getRegistryMainPath(), "LNGameMod");
+			if (!isModDirectory(gamePath + "\\" + regValue))
+				return "";
+
+			return regValue;
+		}
+
+		private static void removeProc(Process[] proc, int count)
+		{
+			if (count > 0)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					proc[i].Kill();
+				}
+			}
+		}
+
+		private void BugreporterMain_Load(object sender, EventArgs e)
+		{
+			modList.Items.Clear();
+			List<string> listOfMods = new List<string>();
+		//	foreach (string dir in Directory.GetDirectories(Directory.GetCurrentDirectory()))
+			foreach (string dir in Directory.GetDirectories(gamePath))
+			{
+				if (isModDirectory(dir))
+				{
+					listOfMods.Add(dir.Substring(dir.LastIndexOf('\\') + 1));
+				}
+			}
+			modList.Items.AddRange(listOfMods.ToArray());
+
+			// Choosing saved mod
+			string choosedGameMod = getModDirectory();
+			int k = 0;
+			bool isModFound = false;
+			foreach (string val in modList.Items)
+			{
+			//	Console.WriteLine(val + " - " + choosedGameMod);
+				if (val == choosedGameMod)
+				{
+					modList.SelectedIndex = k;
+					isModFound = true;
+					break;
+				}
+				k++;
+			}
+			if (!isModFound)
+			{
+				k = 0;
+				foreach (string val in modList.Items)
+				{
+					if (val.IndexOf(choosedGameMod) >= 0)
+					{
+						modList.SelectedIndex = k;
+						break;
+					}
+					k++;
+				}
+			}
+		}
+
+		private void comButton_Click(object sender, EventArgs e)
+		{
+			string choosedMod = modList.SelectedItem.ToString();
+			setRegistryValue("LNGameMod", choosedMod);
+
+		//	Environment.SetEnvironmentVariable("VPROJECT", gamePath + "\\" + getModDirectory());
+			Environment.SetEnvironmentVariable("VPROJECT", gamePath + "\\" + choosedMod);
+		//	string oldCurDir = Directory.GetCurrentDirectory();
+		//	Directory.SetCurrentDirectory(pathToApp);
+		//	Process.Start(pathToApp + "\\" + appExecutable, "-game " + FileSystem.Main.getModDirectory());
+			Process process = new Process();
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.WorkingDirectory = gamePath + "\\" + choosedMod;
+			startInfo.FileName = gamePath + "\\hl2.exe";
+			startInfo.Arguments = "-game " + choosedMod;
+			startInfo.CreateNoWindow = true;
+		//	MessageBox.Show(pathToApp + "; " + (pathToApp + "\\" + appExecutable) + "; " + ("-game " + FileSystem.Main.getModDirectory()));
+		//	Process.Start(startInfo);
+			process.StartInfo = startInfo;
+			process.Start();
+		//	Directory.SetCurrentDirectory(oldCurDir);
+		}
+
+		private void closeBut_Click(object sender, EventArgs e)
+		{
+			Process[] processes = Process.GetProcessesByName("hl2");
+			int procCount = processes.Length;
+			removeProc(processes, procCount);
+		}
+	}
+}
---
+++ MainForm.designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ModLauncher
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.title = new System.Windows.Forms.Label();
+			this.move = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.minimize = new System.Windows.Forms.PictureBox();
+			this.close = new System.Windows.Forms.PictureBox();
+			this.modList = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comButton = new System.Windows.Forms.Button();
+			this.closeBut = new System.Windows.Forms.Button();
+			this.move.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.minimize)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// title
+			// 
+			this.title.AutoSize = true;
+			this.title.BackColor = System.Drawing.Color.Transparent;
+			this.title.Font = new System.Drawing.Font("Tahoma", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(223)))), ((int)(((byte)(200)))));
+			this.title.Location = new System.Drawing.Point(23, 5);
+			this.title.Name = "title";
+			this.title.Size = new System.Drawing.Size(105, 11);
+			this.title.TabIndex = 11;
+			this.title.Text = "LeakNet - Mod Launcher";
+			// 
+			// move
+			// 
+			this.move.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.move.BackColor = System.Drawing.Color.Transparent;
+			this.move.BackgroundImage = global::ModLauncher.Properties.Resources.background003;
+			this.move.Controls.Add(this.pictureBox1);
+			this.move.Controls.Add(this.title);
+			this.move.Controls.Add(this.minimize);
+			this.move.Controls.Add(this.close);
+			this.move.Location = new System.Drawing.Point(0, 0);
+			this.move.Name = "move";
+			this.move.Size = new System.Drawing.Size(352, 21);
+			this.move.TabIndex = 12;
+			this.move.MouseDown += new System.Windows.Forms.MouseEventHandler(this.move_MouseDown);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Image = global::ModLauncher.Properties.Resources.game;
+			this.pictureBox1.Location = new System.Drawing.Point(5, 4);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 20;
+			this.pictureBox1.TabStop = false;
+			// 
+			// minimize
+			// 
+			this.minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.minimize.BackColor = System.Drawing.Color.Transparent;
+			this.minimize.BackgroundImage = global::ModLauncher.Properties.Resources.minimize001;
+			this.minimize.Location = new System.Drawing.Point(325, 6);
+			this.minimize.Name = "minimize";
+			this.minimize.Size = new System.Drawing.Size(7, 9);
+			this.minimize.TabIndex = 13;
+			this.minimize.TabStop = false;
+			this.minimize.Click += new System.EventHandler(this.minimize_Click);
+			// 
+			// close
+			// 
+			this.close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.close.BackColor = System.Drawing.Color.Transparent;
+			this.close.BackgroundImage = global::ModLauncher.Properties.Resources.close001;
+			this.close.Location = new System.Drawing.Point(338, 6);
+			this.close.Name = "close";
+			this.close.Size = new System.Drawing.Size(9, 9);
+			this.close.TabIndex = 9;
+			this.close.TabStop = false;
+			this.close.Click += new System.EventHandler(this.close_Click);
+			// 
+			// modList
+			// 
+			this.modList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+			this.modList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.modList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.modList.ForeColor = System.Drawing.Color.Silver;
+			this.modList.FormattingEnabled = true;
+			this.modList.Location = new System.Drawing.Point(137, 33);
+			this.modList.Name = "modList";
+			this.modList.Size = new System.Drawing.Size(139, 21);
+			this.modList.TabIndex = 13;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(223)))), ((int)(((byte)(200)))));
+			this.label2.Location = new System.Drawing.Point(24, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(107, 13);
+			this.label2.TabIndex = 14;
+			this.label2.Text = "Choose modification:";
+			// 
+			// comButton
+			// 
+			this.comButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(87)))), ((int)(((byte)(82)))));
+			this.comButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.comButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(223)))), ((int)(((byte)(200)))));
+			this.comButton.Location = new System.Drawing.Point(25, 60);
+			this.comButton.Name = "comButton";
+			this.comButton.Size = new System.Drawing.Size(173, 45);
+			this.comButton.TabIndex = 19;
+			this.comButton.Text = "Run modification";
+			this.comButton.UseVisualStyleBackColor = false;
+			this.comButton.Click += new System.EventHandler(this.comButton_Click);
+			// 
+			// closeBut
+			// 
+			this.closeBut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(87)))), ((int)(((byte)(82)))));
+			this.closeBut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.closeBut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(223)))), ((int)(((byte)(200)))));
+			this.closeBut.Location = new System.Drawing.Point(204, 60);
+			this.closeBut.Name = "closeBut";
+			this.closeBut.Size = new System.Drawing.Size(72, 45);
+			this.closeBut.TabIndex = 20;
+			this.closeBut.Text = "Close game";
+			this.closeBut.UseVisualStyleBackColor = false;
+			this.closeBut.Click += new System.EventHandler(this.closeBut_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::ModLauncher.Properties.Resources.background002;
+			this.ClientSize = new System.Drawing.Size(352, 122);
+			this.Controls.Add(this.closeBut);
+			this.Controls.Add(this.comButton);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.modList);
+			this.Controls.Add(this.move);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "LeakNet - Bugreporter";
+			this.Load += new System.EventHandler(this.BugreporterMain_Load);
+			this.move.ResumeLayout(false);
+			this.move.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.minimize)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label title;
+		private System.Windows.Forms.Panel move;
+		private System.Windows.Forms.PictureBox minimize;
+		private System.Windows.Forms.PictureBox close;
+		private System.Windows.Forms.ComboBox modList;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button comButton;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button closeBut;
+	}
+}
